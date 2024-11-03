f7b89754d415f14bbf7fc8506309e69d1718f933
tuhana <tuhana.cat+git@gmail.com>
2024-11-03 19:59:16 +0300

    Document `bouncer-cli`

--- bouncer-cli/src/lib.rs
+++ bouncer-cli/src/lib.rs
@@ -2,6 +2,7 @@ use std::path::PathBuf;
 
 use clap::Parser;
 
+/// CLI options.
 #[derive(Debug, clap::Parser)]
 pub struct Cli {
     #[arg(short, long)]
@@ -9,18 +10,35 @@ pub struct Cli {
 }
 
 impl Cli {
+    /// Parses and validates CLI input.
+    ///
+    /// # Errors
+    ///
+    /// When CLI validation fails, returns [`CliParseError::Validate`].
     pub fn parse_and_validate() -> Result<Self, CliParseError> {
-        Self::validate(&Self::parse()).map_err(From::from)
+        Self::validate(Self::parse()).map_err(From::from)
     }
 
-    fn validate(&self) -> Result<Self, ValidationError> {
+    /// Validates CLI input.
+    ///
+    /// # Errors
+    ///
+    /// When one of the `_validate` suffixed functions (e.g. [`Self::_config_validate`])
+    /// fail, returns [`ValidationError::Config`].
+    fn validate(self) -> Result<Self, ValidationError> {
         Self::_config_validate(&self.config.to_string_lossy())?;
 
-        Ok(Self::parse())
+        Ok(self)
     }
 
-    fn _config_validate(value: &str) -> Result<PathBuf, ConfigValidationError> {
-        let path = PathBuf::from(value);
+    /// Validates the [`Cli::config`] option.
+    ///
+    /// # Errors
+    ///
+    /// If specified `path` doesn't exist, returns
+    /// [`ConfigValidationError::DoesNotExist`].
+    fn _config_validate(path: &str) -> Result<PathBuf, ConfigValidationError> {
+        let path = PathBuf::from(path);
         if !path.exists() {
             return Err(ConfigValidationError::DoesNotExist(
                 path.to_string_lossy().to_string(),
@@ -48,3 +66,5 @@ pub enum ConfigValidationError {
     #[error("Specified config file does not exist: {0}")]
     DoesNotExist(String),
 }
+
+// TODO: Add tests.
